25f08ed727ac2404d3fac796c24fc623b80d41e3
ben-dent <ben@bjmdent.com>
2021-12-02 14:58:16 +0000

    Implementing Day01 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,24 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val numbers: List<Int> = input.map { Integer.parseInt(it) }
+
+        var current = numbers[0]
+        var next: Int
+        var i = 1
+        var n = 0
+
+        while (i < numbers.size) {
+            next = numbers[i]
+
+            if (next > current) {
+                ++n
+            }
+
+            ++i
+            current = next
+        }
+
+        return n
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +27,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
