a18e2b7c1165c3aee60aefa75fc3cb6a675c3be8
ben-dent <ben@bjmdent.com>
2021-12-02 15:05:48 +0000

    Implementing Day01 part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,14 +1,12 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        val numbers: List<Int> = input.map { Integer.parseInt(it) }
-
-        var current = numbers[0]
+    fun part1(input: List<Int>): Int {
+        var current = input[0]
         var next: Int
         var i = 1
         var n = 0
 
-        while (i < numbers.size) {
-            next = numbers[i]
+        while (i < input.size) {
+            next = input[i]
 
             if (next > current) {
                 ++n
@@ -21,15 +19,31 @@ fun main() {
         return n
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        var start = 0
+        var prev = 0
+        var n = 0
+
+        while (start < input.size - 3) {
+            val current = input[start] + input[start + 1] + input[start + 2]
+
+            if (current > prev) {
+                ++n
+            }
+
+            prev = current
+            ++start
+        }
+
+        return n
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = intInput("Day01_test")
     check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = intInput("Day01")
     println(part1(input))
     println(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -8,6 +8,12 @@ import java.security.MessageDigest
 fun readInput(name: String) = File("src", "$name.txt").readLines()
 
 /**
+ * Reads lines from the given input txt file and returns as a List<Int>.
+ */
+
+fun intInput(name: String) = readInput(name).map { Integer.parseInt(it) }
+
+/**
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
